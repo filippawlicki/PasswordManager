63c6b18cb1ce29d5291dda7b26e30fb88e8d40ab
filippawlicki <filip.pawlicki77@gmail.com>
2024-05-22 09:21:14 +0200

    add header to the .sh file

--- passwordmanager.sh
+++ passwordmanager.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
+# Author           : Filip Pawlicki ( s198371@student.pg.edu.pl )
+# Created On       : 16.04.2024
+# Last Modified By : Filip Pawlicki ( s198371@student.pg.edu.pl )
+# Last Modified On : 15.05.2024 
+# Version          : 1.0
+#
+# Description      :
+#
+# Title : Password Manager
+#
+# Packages:
+#
+# 	openssl: For encryption and decryption
+# 	zenity: For creating a user interface
+#	xclip: For copy passwords to clipboard
+#	awk: For parsing user input
+#
+# Functions:
+#
+# 	Password encryption
+# 	Generating random passwords
+# 	Getting passwords
+# 	Updating passwords
+# 	Deleting passwords
+#
+# Display menu with options:
+#
+# 	Save new password
+# 	Get password (Select from list)
+# 	Update password
+# 	Delete password
+# 	Change master password
+# 	Generate new password
+# 	Exit
+#
+# Licensed under GPL (see /usr/share/common-licenses/GPL for more details
+# or contact # the Free Software Foundation for a copy)
+
 LOGGED=0
 # Set the password to decrypt the master password
 DECRYPTPASS="dummy"
